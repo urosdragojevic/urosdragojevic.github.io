cdf298d089503af268ce995d81fd80b997c90564
Uros Dragojevic <48795517+urosdragojevic@users.noreply.github.com>
2024-01-26 21:45:28 +0100

    Revert "Fix back to top arrow" (#30)

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -25,7 +25,7 @@ export const ui = {
         'operations.title': 'Operations',
         'operations.subtitle': 'Posts on topics regarding operations.',
         'navCtl.backToHomepage': 'Back to homepage',
-        'navCtl.backToTop': '↑',
+        'navCtl.backToTop': '🠭',
     },
     sr: {
         'home.subtitle': 'Pročitaj najnovije postove.',
@@ -44,6 +44,6 @@ export const ui = {
         'operations.title': 'Operacije',
         'operations.subtitle': 'Postovi vezani za operacije.',
         'navCtl.backToHomepage': 'Nazad na početnu stranicu',
-        'navCtl.backToTop': '↑',
+        'navCtl.backToTop': '🠭',
     },
 } as const;
